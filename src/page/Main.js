bddd91d4fe7a682a5533622bae7f1f155140d5e9
Marmin <marmin.dev@gmail.com>
2024-09-12 16:08:19 +0900

    seo 설정

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { GetIn, GirlDiv, Text } from "../Module/MainModule";
+import { Helmet } from "react-helmet";
 
 // 메인 화면
 const Main = () => {
@@ -12,12 +13,25 @@ const Main = () => {
   }, []);
 
   return (
-    <GirlDiv>
-      <Text>
-        SUMSUMAI
-        <GetIn href={inUrl}>수희와 대화하기</GetIn>
-      </Text>
-    </GirlDiv>
+    <>
+      <Helmet>
+        <title>썸썸AI</title>
+        <meta name="description" content="수희와 대화하기" />
+        <meta
+          name="keywords"
+          content="Sumsumai, sumsumai, 수희 대화, sumsum, 썸썸AI, 썸썸"
+        />
+        <meta property="og:title" content="썸썸AI" />
+        <meta property="og:description" content="수희와 대화하기" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+      <GirlDiv>
+        <Text>
+          SUMSUMAI
+          <GetIn href={inUrl}>수희와 대화하기</GetIn>
+        </Text>
+      </GirlDiv>
+    </>
   );
 };
 
